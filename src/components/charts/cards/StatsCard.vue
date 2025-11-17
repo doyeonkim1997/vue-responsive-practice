f41d14f2161b692f9308f9ca589ee6f350f174cd
Doyeon Kim <168827313+doyeonkim1997@users.noreply.github.com>
2025-11-17 14:06:21 +0900

    feat: 공통 카드 및 레이아웃 컴포넌트 분리

---
+++ src/components/charts/cards/StatsCard.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+interface Props {
+  label: string;
+  value: string | number;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="bg-white p-5 rounded-lg shadow-sm border">
+    < class="text-lg mb-2{{ props.label }}></h2>
+    <p class="text-2x1 font-semibold">
+      {{ props.value }}
+    </p>
+  </div>
+</template>
