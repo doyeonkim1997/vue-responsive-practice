54dfcf4ae891eef0fadeb864e8e02b4c98398641
Doyeon Kim <168827313+doyeonkim1997@users.noreply.github.com>
2025-11-17 14:05:35 +0900

    refactor: NotFound 페이지 레이아웃 개선 및 GlobalContainer 적용

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalContainer from "@/components/common/GlobalContainer.vue";
+</script>
 
 <template>
-  <div class="h-[60vh] flex flex-col items-center justify-center text-gray-600">
-    <h1 class="mb-4 text-5xl font-semibold">404</h1>
-    <p class="mb-7 text-lg font-normal">페이지를 찾을 수 없습니다.</p>
-    <RouterLink to="/" class="text-blue-600">홈으로 돌아가기</RouterLink>
-  </div>
+  <GlobalContainer class="space-y-8">
+    <div
+      class="h-[60vh] flex flex-col items-center justify-center text-gray-600"
+    >
+      <h1 class="mb-4 text-5xl font-semibold">404</h1>
+      <p class="mb-7 text-lg font-normal">페이지를 찾을 수 없습니다.</p>
+      <RouterLink to="/" class="text-blue-600">홈으로 돌아가기</RouterLink>
+    </div>
+  </GlobalContainer>
 </template>
