4ab7dd947101e653e3c707a893bf6f85281b6d30
Doyeon Kim <168827313+doyeonkim1997@users.noreply.github.com>
2025-11-18 15:32:43 +0900

    chore: 대시보드용 mock 데이터 추가

---
+++ src/mocks/dashboard.ts
@@ -0,0 +1,8 @@
+export const weeklyVisitors = [120, 135, 150, 160, 180, 200, 220];
+
+export const weeklySales = {
+  labels: ["월", "화", "수", "목", "금", "토", "일"],
+  data: [140, 160, 170, 180, 200, 220, 250],
+};
+
+export const weeklySignup = [150, 170, 180, 200, 230, 250, 300];
