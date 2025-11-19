67292f2dd13eeee7f74a971d926b35b4aa0987a8
Doyeon Kim <168827313+doyeonkim1997@users.noreply.github.com>
2025-11-19 13:58:29 +0900

    fix: 파일 중복으로 인한 내용 교체

--- src/components/charts/cards/StatsCard.vue
+++ src/components/charts/cards/StatsCard.vue
@@ -1,15 +1,28 @@
-<script lang="ts">
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
 interface Props {
   label: string;
   value: string | number;
+  to?: string;
 }
 
 const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="bg-white p-5 rounded-lg shadow-sm border flex-1 min-w-[200px]">
-    <h2 class="text-lg mb-2 font-semibold">{{ label }}</h2>
-    <p class="text-2xl font-bold">{{ value }}</p>
+  <RouterLink v-if="props.to" :to="props.to" class="block focus:outline-none">
+    <div class="bg-white p-5 rounded-lg shadow-sm border cursor-pointer">
+      <h2 class="text-lg mb-2">{{ props.label }}</h2>
+      <p class="text-2xl font-semibold">
+        {{ props.value }}
+      </p>
+    </div>
+  </RouterLink>
+  <div v-else class="bg-white p-5 rounded-lg shadow-sm border">
+    <h2 class="text-lg mb-2">{{ props.label }}</h2>
+    <p class="text-2xl font-semibold">
+      {{ props.value }}
+    </p>
   </div>
 </template>
